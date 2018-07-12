d62ce0410ba3869aa541f2807a214b48b30580eb
justinmetivier <justinmetivier@crescentschool.org>
2018-07-12 00:40:30 -0400

    Map Addition
    Added map transition but is only working on a physical phone for some reason

--- AaronHelpsMe/app/src/main/java/com/example/ben/aaronhelpsme/Main2Activity.java
+++ AaronHelpsMe/app/src/main/java/com/example/ben/aaronhelpsme/Main2Activity.java
@@ -3,11 +3,16 @@ package com.example.ben.aaronhelpsme;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
     private TextView Textv;
+    private Button next;
 
 
     @Override
@@ -17,8 +22,10 @@ public class Main2Activity extends AppCompatActivity {
 
 
         Textv = (TextView)findViewById(R.id.tvd);
+
         Intent iin= getIntent();
         Bundle b = iin.getExtras();
+
 
         if(b!=null)
         {
@@ -26,5 +33,26 @@ public class Main2Activity extends AppCompatActivity {
             Textv.setText("Welcome " + j);
         }
 
+        addListenerOnButton();
+
     }
+    public void addListenerOnButton() {
+
+        next = (Button) findViewById(R.id.nextbtn);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                    startActivity(new Intent(Main2Activity.this, MapsActivity.class));
+
+
+
+
+
+            }
+        });
+
+    }
+
 }
